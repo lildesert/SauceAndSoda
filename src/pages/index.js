a7ab4b3439a0864f855b08dbd0525d7076869c8d
lildesert <julien.bistoquet@gmail.com>
2019-01-09 17:52:04 +0100

    use fr locale for date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export const query = graphql`
           id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "DD MMMM, YYYY", locale: "fr")
             category
             coverImage {
               publicURL
